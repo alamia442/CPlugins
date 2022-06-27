01d947337fea41d236520f36b7ef02c7874bd2d9
alamia442 <61982500+alamia442@users.noreply.github.com>
2022-06-28 01:26:07 +0700

    Update __main__.py

--- plugins/utils/ssweb/__main__.py
+++ plugins/utils/ssweb/__main__.py
@@ -15,6 +15,7 @@ from re import match
 import aiofiles
 from fake_headers import Headers
 from selenium import webdriver
+from PIL import Image
 
 from userge import userge, Message, config
 from .. import webss
@@ -75,4 +76,109 @@ async def _webss(message: Message):
                                      reply_to_message_id=message_id)
     )
     os.remove(file_path)
+    driver.quit()
+
+
+@userge.on_cmd("tbo", about={'header': "Get snapshot of IMDB Top Box Office (US)"})
+async def _tboss(message: Message):
+    if webss.GOOGLE_CHROME_BIN is None:
+        await message.edit("`need to install Google Chrome. Module Stopping`", del_in=5)
+        return
+    await message.edit("`Processing ...`")
+    options = webdriver.ChromeOptions()
+    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    options.add_argument('--force-dark-mode')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument("--test-type")
+    options.add_argument("--headless")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--no-sandbox")
+    options.add_argument('--disable-gpu')
+
+    driver = webdriver.Chrome(chrome_options=options, executable_path=os.environ.get("GOOGLE_CHROME_DRIVER"), )
+    driver.get('https://m.imdb.com/')
+    height = driver.execute_script(
+         "return Math.max(document.body.scrollHeight, document.body.offsetHeight, "
+         "document.documentElement.clientHeight, document.documentElement.scrollHeight, "
+         "document.documentElement.offsetHeight);")
+    driver.set_window_size(375, height + 125)
+    driver.maximize_window()
+    await message.edit("`Generating screenshot of IMDB Top Box Office (US)`")
+    element = driver.find_elements_by_xpath('//*[@id="__next"]/main/div/div[3]/div[7]/section[2]')[-1]
+    element.screenshot("TBO.png")
+    driver.close()
+
+    await asyncio.gather(
+        message.delete(),
+        message.client.send_photo(chat_id=message.chat.id,
+                                     photo="TBO.png",
+                                     caption="**Top Box Office (US)**",
+                                     reply_to_message_id=message_id)
+    )
+    os.remove("TBO.png")
+    driver.quit()
+
+
+@userge.on_cmd("post", about={'header': "Get snapshot of IMDB Movie info"})
+async def _tboss(message: Message):
+    if webss.GOOGLE_CHROME_BIN is None:
+        await message.edit("`need to install Google Chrome. Module Stopping`", del_in=5)
+        return
+    movie_name = message.input_str
+    await message.edit("`Processing ...`")
+    options = webdriver.ChromeOptions()
+    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    options.add_argument('--force-dark-mode')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument("--test-type")
+    options.add_argument("--headless")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--no-sandbox")
+    options.add_argument('--disable-gpu')
+
+    driver = webdriver.Chrome(chrome_options=options, executable_path=os.environ.get("GOOGLE_CHROME_DRIVER"), )
+    driver.get(f'https://m.imdb.com/title/{movie_name}')
+    height = driver.execute_script(
+         "return Math.max(document.body.scrollHeight, document.body.offsetHeight, "
+         "document.documentElement.clientHeight, document.documentElement.scrollHeight, "
+         "document.documentElement.offsetHeight);")
+    driver.set_window_size(768, height + 125)
+    driver.maximize_window()
+    await message.edit("`Generating screenshot of IMDB Movie info`")
+    head = driver.find_elements_by_xpath('//*[@id="__next"]/main/div/section[1]/section')[-1]
+    foot = driver.find_elements_by_xpath('//*[@id="__next"]/main/div/section[1]/div/section/div/div[1]/section[1]')[-1]
+
+    head.screenshot("dark_h.png")
+    foot.screenshot("dark_f.png")
+    
+    img_list = ['dark_h.png', 'dark_f.png']
+    imgs = [Image.open(i) for i in img_list]
+    min_img_width = min(i.width for i in imgs)
+
+    total_height = 0
+    for i, img in enumerate(imgs):
+        if img.width > min_img_width:
+            imgs[i] = img.resize((min_img_width, int(img.height / img.width * min_img_width)), Image.ANTIALIAS)
+            total_height += imgs[i].height
+            img_merge = Image.new(imgs[0].mode, (min_img_width, total_height))
+    y = 0
+    for img in imgs:
+        img_merge.paste(img, (0, y))
+        y += img.height
+    img_merge.save('Movie.png')
+
+    driver.close()
+
+    await asyncio.gather(
+        message.delete(),
+        message.client.send_photo(chat_id=message.chat.id,
+                                     photo="Movie.png",
+                                     caption="👇",
+                                     reply_to_message_id=message_id)
+    )
+    os.remove("dark_h.png")
+    os.remove("dark_f.png")
+    os.remove("Movie.png")
     driver.quit()
