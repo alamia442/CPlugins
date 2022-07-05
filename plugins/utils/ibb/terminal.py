31f41c75dcfd9cc947eb3736c7053ed90e52a686
alamia442 <61982500+alamia442@users.noreply.github.com>
2022-07-06 04:36:34 +0700

    Update terminal.py

--- plugins/utils/ibb/terminal.py
+++ plugins/utils/ibb/terminal.py
@@ -2,59 +2,109 @@ import asyncio
 
 
 class Terminal:
+    """ live update term class """
+
     def __init__(self, process: asyncio.subprocess.Process) -> None:
         self._process = process
-        self._stdout = b''
-        self._stderr = b''
-        self._stdout_line = b''
-        self._stderr_line = b''
+        self._line = b''
+        self._output = b''
+        self._init = asyncio.Event()
+        self._is_init = False
+        self._cancelled = False
         self._finished = False
+        self._loop = asyncio.get_running_loop()
+        self._listener = self._loop.create_future()
 
-    def cancel(self) -> None:
-        self._process.kill()
+    @property
+    def line(self) -> str:
+        return self._by_to_str(self._line)
+
+    @property
+    def output(self) -> str:
+        return self._by_to_str(self._output)
+
+    @staticmethod
+    def _by_to_str(data: bytes) -> str:
+        return data.decode('utf-8', 'replace').strip()
+
+    @property
+    def cancelled(self) -> bool:
+        return self._cancelled
 
     @property
     def finished(self) -> bool:
         return self._finished
 
-    @property
-    def read_line(self) -> str:
-        return (self._stdout_line + self._stderr_line).decode('utf-8').rstrip()
-        #return (self._stdout + self._stderr).decode('utf-8').strip()
+    async def init(self) -> None:
+        await self._init.wait()
 
-    @property
-    def get_output(self) -> str:
-        return (self._stdout + self._stderr).decode('utf-8').rstrip()
+    async def wait(self, timeout: int) -> None:
+        self._check_listener()
+        try:
+            await asyncio.wait_for(self._listener, timeout)
+        except asyncio.TimeoutError:
+            pass
 
-    async def _read_stdout(self) -> None:
-        while True:
-            #line = await self._process.stdout.readline()
-            line = await self._process.stdout.read(n=1024)
-            if line:
-                self._stdout_line = line
-                self._stdout += line
-            else:
-                break
+    def _check_listener(self) -> None:
+        if self._listener.done():
+            self._listener = self._loop.create_future()
 
-    async def _read_stderr(self) -> None:
-        while True:
-            #line = await self._process.stderr.readline()
-            line = await self._process.stderr.read(n=1024)
-            if line:
-                self._stderr_line = line
-                self._stderr += line
-            else:
-                break
-
-    async def worker(self) -> None:
-        await asyncio.wait([self._read_stdout(), self._read_stderr()])
-        await self._process.wait()
-        self._finished = True
+    def cancel(self) -> None:
+        if self._cancelled or self._finished:
+            return
+        killpg(getpgid(self._process.pid), SIGKILL)
+        self._cancelled = True
 
     @classmethod
-    async def execute(cls, cmd: str):
-        process = await asyncio.create_subprocess_shell(
-            cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    async def execute(cls, cmd: str) -> 'Term':
+        kwargs = dict(
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE)
+        if setsid:
+            kwargs['preexec_fn'] = setsid
+        process = await asyncio.create_subprocess_shell(cmd, **kwargs)
         t_obj = cls(process)
-        asyncio.get_event_loop().create_task(t_obj.worker())
+        t_obj._start()
         return t_obj
+
+    def _start(self) -> None:
+        self._loop.create_task(self._worker())
+
+    async def _worker(self) -> None:
+        if self._cancelled or self._finished:
+            return
+        await asyncio.wait([self._read_stdout(), self._read_stderr()])
+        await self._process.wait()
+        self._finish()
+
+    async def _read_stdout(self) -> None:
+        await self._read(self._process.stdout)
+
+    async def _read_stderr(self) -> None:
+        await self._read(self._process.stderr)
+
+    async def _read(self, reader: asyncio.StreamReader) -> None:
+        while True:
+            line = await reader.readline()
+            if not line:
+                break
+            self._append(line)
+
+    def _append(self, line: bytes) -> None:
+        self._line = line
+        self._output += line
+        self._check_init()
+
+    def _check_init(self) -> None:
+        if self._is_init:
+            return
+        self._loop.call_later(1, self._init.set)
+        self._is_init = True
+
+    def _finish(self) -> None:
+        if self._finished:
+            return
+        self._init.set()
+        self._finished = True
+        if not self._listener.done():
+            self._listener.set_result(None)
