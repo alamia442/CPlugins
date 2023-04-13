4ed93445fb3fe0263667753ca298c719fc9f938e
alamia442 <61982500+alamia442@users.noreply.github.com>
2023-04-13 09:54:49 +0700

    Update __main__.py

--- plugins/utils/ssweb/__main__.py
+++ plugins/utils/ssweb/__main__.py
@@ -99,13 +99,10 @@ async def _tboss(message: Message):
     driver = webdriver.Chrome(chrome_options=options, executable_path=f"{ssweb.GOOGLE_CHROME_DRIVER}")
     driver.get('https://m.imdb.com/')
     (driver.page_source).encode('utf-8')
-    height = driver.execute_script(
-         "return Math.max(document.body.scrollHeight, document.body.offsetHeight, "
-         "document.documentElement.clientHeight, document.documentElement.scrollHeight, "
-         "document.documentElement.offsetHeight);")
-    driver.set_window_size(375, height + 125)
+    driver.execute_script("window.scrollTo(0, 7300);")
+    driver.set_window_size(375, 7300)
     driver.maximize_window()
-    wait_for = height / 1000
+    wait_for = 7300 / 1000
     await message.edit("`Generating screenshot of IMDB Top Box Office (US)`")
     await asyncio.sleep(int(wait_for))
     element = driver.find_elements_by_xpath('//*[@id="__next"]/main/div/div[3]/div[7]/div/section[2]/div')
@@ -166,8 +163,8 @@ async def _postss(message: Message):
     await message.edit("`Generating screenshot of IMDB Movie info`")
     await asyncio.sleep(int(wait_for))
     if movie_name.startswith('tt'):
-        head = driver.find_elements_by_xpath('//*[@id="__next"]/main/div/section[1]/section')[0]
-        foot = driver.find_elements_by_xpath('//*[@id="__next"]/main/div/section[1]/div/section/div/div[1]/section[9]')[0]
+        head = driver.find_elements_by_xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[2]')[-1]
+        foot = driver.find_elements_by_xpath('//*[@id="__next"]/main/div/section[1]/div/section/div/div[1]/section[5]')[-1]
         head.screenshot("dark_h.png")
         foot.screenshot("dark_f.png")
         images_list = ['dark_h.png', 'dark_f.png']
