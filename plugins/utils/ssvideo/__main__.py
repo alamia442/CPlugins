8facef6d9aa7dc0040230b77f29b09b6c18a9e1f
alamia442 <61982500+alamia442@users.noreply.github.com>
2023-05-31 01:03:30 +0700

    Update __main__.py

--- plugins/utils/ssvideo/__main__.py
+++ plugins/utils/ssvideo/__main__.py
@@ -1,74 +1,59 @@
-""" generate screenshot """
-
-# Copyright (C) 2020-2022 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
-#
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/UsergeTeam/Userge/blob/master/LICENSE >
-#
-# All rights reserved.
-
-
 import os
 import asyncio
-
+from pathlib import Path
 
 from userge import userge, Message, config
-from userge.utils import progress
+from userge.utils.exceptions import ProcessCanceled
+
+from .. import ssvideo
+
+LOGGER = userge.getLogger(__name__)
 
 
-@userge.on_cmd("ssv", about={
+@userge.on_cmd("ss", about={
     'header': "Screen Shot Generator",
-    'description': "Generate Random Screen Shots from any video "
-                   " **[NOTE: If no frame count is passed, default"
-                   " value for number of ss is 10. ",
-    'usage': "{tr}ssv [No of SS (optional)] [Path or reply to Video]"})
+    'usage': "{tr}ss [No of SS (optional)] [url | path | reply to telegram media]"},
+    check_downpath=True)
 async def ss_gen(message: Message):
-    replied = message.reply_to_message
-    vid_loc = ''
-    ss_c = 5
+    """ download from tg and url """
     should_clean = False
-    await message.edit("Checking you Input?🧐🤔😳")
-    if message.input_str:
+    if message.reply_to_message:
+        resource = message.reply_to_message
+    elif message.input_str:
         if ' ' in message.input_str:
-            ss_c, vid_loc = message.input_str.split(" ", 1)
+            ss_c, resource = message.input_str.split(" ", 1)
         else:
             try:
                 ss_c = int(message.input_str)
             except ValueError:
-                vid_loc = message.input_str
+                resource = message.input_str
+    else:
+        await message.err("nothing found to download")
+        return
 
-    if not vid_loc and replied:
-        if not (
-            replied.video
-            or replied.animation
-            or (replied.document and "video" in replied.document.mime_type)
-        ):
-            await message.edit("I doubt it is a video")
-            return
-        await message.edit("Downloading Video to my Local")
-        vid = await message.client.download_media(
-            message=replied,
-            file_name=config.Dynamic.DOWN_PATH,
-            progress=progress,
-            progress_args=(message, "Downloading🧐? W8 plox")
-        )
-        vid_loc = os.path.join(config.Dynamic.DOWN_PATH, os.path.basename(vid))
-        should_clean = True
-    await message.edit("Compiling Resources")
+    if os.path.isfile(resource):
+        command = f"vcsi -g {ss_c}x{ss_c} {resource} -o /ss.png"
+    else:
+        try:
+            dl_loc, d_in = await download.handle_download(message, resource)
+        except ProcessCanceled:
+            await message.canceled()
+        except Exception as e_e:  # pylint: disable=broad-except
+            await message.err(str(e_e))
+        else:
+            should_clean = True
+            command = f"vcsi -g {ss_c}x{ss_c} {dl_loc} -o /ss.png"
+
     try:
-        command = f"vcsi -g {ss_c}x{ss_c} {vid} -o ss.png"
         os.system(command)
-    except Exception:
-        command = f"vcsi -g {ss_c}x{ss_c} {vid_loc} -o ss.png"
-        os.system(command)
-    dir = config.Dynamic.DOWN_PATH
-    doc = f"{dir}/ss.png"
+    except Exception as e_e:
+        await message.err(str(e_e))
+
     await message.client.send_document(
         chat_id=message.chat.id,
-        document=doc)
+        document="/ss.png")
     if should_clean:
-        os.remove(vid_loc)
-        os.remove(doc)
+        os.remove(dl_loc)
+        os.remove("/ss.png")
     await asyncio.sleep(0.5)
     await message.edit("Done.")
