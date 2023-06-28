b3c84cdcb6fe8c1b067c72d79e8c59bcdd16b21e
alamia442 <61982500+alamia442@users.noreply.github.com>
2023-06-28 17:02:00 +0700

    Update __main__.py

--- plugins/utils/ibb/__main__.py
+++ plugins/utils/ibb/__main__.py
@@ -152,3 +152,113 @@ async def my_term_(message: Message):
     out_data = f"{output}\n<pre>{t_obj.output}</pre>\n"
     await message.edit_or_send_as_file(
         out_data, as_raw=as_raw, parse_mode=enums.ParseMode.HTML, filename="term.txt", caption=cmd)
+
+class Term:
+    """ live update term class """
+
+    def __init__(self, process: asyncio.subprocess.Process) -> None:
+        self._process = process
+        self._line = b''
+        self._output = b''
+        self._init = asyncio.Event()
+        self._is_init = False
+        self._cancelled = False
+        self._finished = False
+        self._loop = asyncio.get_running_loop()
+        self._listener = self._loop.create_future()
+
+    @property
+    def line(self) -> str:
+        return self._by_to_str(self._line)
+
+    @property
+    def output(self) -> str:
+        return self._by_to_str(self._output)
+
+    @staticmethod
+    def _by_to_str(data: bytes) -> str:
+        return data.decode('utf-8', 'replace').strip()
+
+    @property
+    def cancelled(self) -> bool:
+        return self._cancelled
+
+    @property
+    def finished(self) -> bool:
+        return self._finished
+
+    async def init(self) -> None:
+        await self._init.wait()
+
+    async def wait(self, timeout: int) -> None:
+        self._check_listener()
+        try:
+            await asyncio.wait_for(self._listener, timeout)
+        except asyncio.TimeoutError:
+            pass
+
+    def _check_listener(self) -> None:
+        if self._listener.done():
+            self._listener = self._loop.create_future()
+
+    def cancel(self) -> None:
+        if self._cancelled or self._finished:
+            return
+        killpg(getpgid(self._process.pid), SIGKILL)
+        self._cancelled = True
+
+    @classmethod
+    async def execute(cls, cmd: str) -> 'Term':
+        kwargs = dict(
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE)
+        if setsid:
+            kwargs['preexec_fn'] = setsid
+        if sh := which(os.environ.get("USERGE_SHELL", "bash")):
+            kwargs['executable'] = sh
+        process = await asyncio.create_subprocess_shell(cmd, **kwargs)
+        t_obj = cls(process)
+        t_obj._start()
+        return t_obj
+
+    def _start(self) -> None:
+        self._loop.create_task(self._worker())
+
+    async def _worker(self) -> None:
+        if self._cancelled or self._finished:
+            return
+        await asyncio.wait([self._read_stdout(), self._read_stderr()])
+        await self._process.wait()
+        self._finish()
+
+    async def _read_stdout(self) -> None:
+        await self._read(self._process.stdout)
+
+    async def _read_stderr(self) -> None:
+        await self._read(self._process.stderr)
+
+    async def _read(self, reader: asyncio.StreamReader) -> None:
+        while True:
+            line = await reader.readline(n=1024)
+            if not line:
+                break
+            self._append(line)
+
+    def _append(self, line: bytes) -> None:
+        self._line = line
+        self._output += line
+        self._check_init()
+
+    def _check_init(self) -> None:
+        if self._is_init:
+            return
+        self._loop.call_later(1, self._init.set)
+        self._is_init = True
+
+    def _finish(self) -> None:
+        if self._finished:
+            return
+        self._init.set()
+        self._finished = True
+        if not self._listener.done():
+            self._listener.set_result(None)
