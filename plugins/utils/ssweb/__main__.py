80cd76efe288be3c22b4627e4a212fbf82546808
alamia442 <61982500+alamia442@users.noreply.github.com>
2023-06-30 12:07:34 +0700

    Update __main__.py

--- plugins/utils/ssweb/__main__.py
+++ plugins/utils/ssweb/__main__.py
@@ -174,7 +174,7 @@ async def _postss(message: Message):
     await asyncio.sleep(int(wait_for))
     #logging.info(driver.page_source)
     if movie_name.startswith('tt'):
-        head = driver.find_elements_by_xpath('//*[@id="__next"]/main/div/section[1]/section')
+        head = driver.find_elements_by_xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section')
         foot = driver.find_elements_by_xpath('//*[@id="__next"]/main/div/section[1]/div/section/div/div[1]/section[1]')
         logging.info(head)
         head.screenshot("dark_h.png")
